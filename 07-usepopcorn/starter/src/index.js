89a4efe9c1bb3c9272165fe5146e58245914b2eb
Ogunsolu olawale <63315246+ogunsolu007@users.noreply.github.com>
2024-12-17 20:47:28 +0100

    popcorn useeffect

--- 07-usepopcorn/starter/src/index.js
+++ 07-usepopcorn/starter/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={5} color="red" className="test" /> */}
   </React.StrictMode>
 );
-
